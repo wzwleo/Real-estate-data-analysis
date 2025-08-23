d2f5d2d274442f45fd8d5fc4d945d387792c7db2
wzwleo <leo20050312@gmail.com>
2025-08-24 02:46:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def main():
     st.sidebar.title("側邊欄")
 
     with st.expander("操作區"):
-        st.button("按鈕")
-        st.text_input("輸入框")
+
 
 if __name__ == "__main__":
 
     main()
+
 
 
 
