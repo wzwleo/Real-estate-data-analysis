cfb79b5f8482d44330d218c94c1eef8931dffc68
wzwleo <leo20050312@gmail.com>
2025-08-24 02:30:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import streamlit as st
 #from (檔案名稱) import (函式名稱)
 
 def main():
+    st.set_page_config(layout="wide")
+    
     st.title("🏠AI購屋分析")
     st.sidebar.title("側邊欄")
-    
+
+
+
 if __name__ == "__main__":
 
     main()
+
 
 
 
