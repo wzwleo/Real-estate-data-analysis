5b164a2f023d463ee9fc5edcebd7da966a99b4ec
wzwleo <leo20050312@gmail.com>
2025-08-24 02:27:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import streamlit as st
 #from (檔案名稱) import (函式名稱)
 
 def main():
-    st.title("🏠購屋分析")
-
+    st.title("🏠AI購屋分析")
+    st.sidebar.title("側邊欄")
+    
 if __name__ == "__main__":
 
     main()
+
 
 
