d498b2486e82292f174a92fda14fc7541403a1e2
wzwleo <leo20050312@gmail.com>
2025-08-24 02:47:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def main():
     st.title("🏠AI購屋分析")
     st.sidebar.title("側邊欄")
 
-    with st.expander("操作區"):
-
+    with st.sidebar.expander("操作區"):
+        st.button("按鈕")
+        st.text_input("輸入框")
 
 if __name__ == "__main__":
 
     main()
+
 
 
 
