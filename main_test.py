71950cb0fc0db5fb748f0b61bca6f4aa679f91f0
wzwleo <leo20050312@gmail.com>
2025-08-28 00:51:41 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from sidebar import render_sidebar
-from pages.home_page import render_home_page
-from pages.search_page import render_search_page
-from pages.analysis_page import render_analysis_page
+from page_modules.home_page import render_home_page
+from page_modules.search_page import render_search_page
+from page_modules.analysis_page import render_analysis_page
 
 def main():
     """
