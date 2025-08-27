0ff94c8b12d69014ee7859ac06c07958391373d7
wzwleo <leo20050312@gmail.com>
2025-08-28 00:15:35 +0800

    Create ui_home.py

---
+++ modules/ui_home.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def render_home():
+    st.title("🏠AI購屋分析")
+    st.write("👋歡迎來到房地產分析系統")
+    st.write("以下是使用說明：")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.form("search"):
+            st.subheader("🔍 搜尋頁面")
+            st.write("第一步：阿對對對 就是這樣 嗯嗯嗯 沒錯沒錯")
+            search_bt = st.form_submit_button("開始")
+            if search_bt:
+                st.session_state.current_page = 'search'
+
+        with st.form("form2"):
+            st.subheader("表單 2")
+            submit2 = st.form_submit_button("提交")
+            if submit2:
+                st.write("施工中...")
+
+    with col2:
+        with st.form("analysis"):
+            st.subheader("📊 分析頁面")
+            st.write("第二步：阿對對對 就是這樣 嗯嗯嗯 沒錯沒錯")
+            analysis_bt = st.form_submit_button("開始")
+            if analysis_bt:
+                st.session_state.current_page = 'analysis'
+
+        with st.form("form4"):
+            st.subheader("表單 4")
+            submit4 = st.form_submit_button("提交")
+            if submit4:
+                st.write("施工中...")
