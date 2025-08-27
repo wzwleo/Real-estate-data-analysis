ed69e309ba65831fb0f96c6ebaf8fecdac9f037f
AeTen07 <w101882048@gmail.com>
2025-08-28 04:20:36 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ from sidebar import render_sidebar
 from page_modules.home_page import render_home_page
 from page_modules.search_page import render_search_page
 from page_modules.analysis_page import render_analysis_page
-
+from page_modules.compare_page import render_compare_page 
 
 def main():
     """
@@ -25,7 +25,8 @@ def main():
         render_search_page()
     elif st.session_state.current_page == 'analysis':
         render_analysis_page()
-   
+    elif st.session_state.current_page == 'compare':
+        render_compare_page()
 
 if __name__ == "__main__":
     main()
