ba70eb1eca6aff93fc92c3e314153af0d8327910
wzwleo <leo20050312@gmail.com>
2025-09-19 04:31:42 +0800

    Update analysis_page.py

--- page_modules/analysis_page.py
+++ page_modules/analysis_page.py
@@ -7,6 +7,7 @@ def render_analysis_page():
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 1, 1, 1])
     with col7:
         analysis_scope = st.selectbox(
+            "分析類別",
             ["⭐收藏類別", "已售出房產"],
             key="analysis_scope"
         )
