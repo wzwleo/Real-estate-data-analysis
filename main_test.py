51e61552239266f6552d74cf8623f86413483692
wzwleo <leo20050312@gmail.com>
2025-09-19 04:04:55 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -3,7 +3,6 @@ from sidebar import render_sidebar
 from page_modules.home_page import render_home_page
 from page_modules.search_page import render_search_page
 from page_modules.analysis_page import render_analysis_page
-from page_modules.compare_page import render_compare_page 
 
 def main():
     """
