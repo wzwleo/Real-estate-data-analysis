312e258a5b4b80e4424790a97bb8774f2601b739
wzwleo <leo20050312@gmail.com>
2025-09-19 04:32:03 +0800

    Update analysis_page.py

--- page_modules/analysis_page.py
+++ page_modules/analysis_page.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 def render_analysis_page():
     st.title("📊 分析頁面")
-    st.markdown("---")
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 1, 1, 1])
     with col7:
         analysis_scope = st.selectbox(
@@ -11,3 +10,5 @@ def render_analysis_page():
             ["⭐收藏類別", "已售出房產"],
             key="analysis_scope"
         )
+    st.markdown("---")
+
