77d0860d80b33e1d34b19c7d96106674912ad4ff
AeTen07 <w101882048@gmail.com>
2025-09-20 13:57:57 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -4,6 +4,7 @@ from page_modules.home_page import render_home_page
 from page_modules.search_page import render_search_page
 from page_modules.analysis_page import render_analysis_page
 
+
 def main():
     """
     主應用程式入口
