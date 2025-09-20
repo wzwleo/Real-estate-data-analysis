f26867684007363db1ea01f0051d52d59a79ef5a
AeTen07 <w101882048@gmail.com>
2025-09-20 13:59:27 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ from sidebar import render_sidebar
 from page_modules.home_page import render_home_page
 from page_modules.search_page import render_search_page
 from page_modules.analysis_page import render_analysis_page
-
+from page_modules.house_compare_page import 
 
 def main():
     """
