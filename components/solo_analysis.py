4a0904649b5fe355bdd4e6ef5bc0086cb8cf775e
wzwleo <leo20050312@gmail.com>
2025-09-27 12:48:44 +0800

    Update solo_analysis.py

--- components/solo_analysis.py
+++ components/solo_analysis.py
@@ -6,12 +6,12 @@ def tab1_module():
     fav_df = get_favorites_data()
     if fav_df.empty:
             st.info("⭐ 尚未有收藏房產，無法比較")
-        else:
-            options = fav_df['標題'] + " | " + fav_df['地址']
-            col1, col2 = st.columns(2)
-            with col1:
-                choice_a = st.selectbox("選擇房屋 A", options, key="compare_a")
-            with col2:
-                choice_b = st.selectbox("選擇房屋 B", options, key="compare_b")
+    else:
+        options = fav_df['標題'] + " | " + fav_df['地址']
+        col1, col2 = st.columns(2)
+        with col1:
+            choice_a = st.selectbox("選擇房屋 A", options, key="compare_a")
+        with col2:
+            choice_b = st.selectbox("選擇房屋 B", options, key="compare_b")
     
 
