f0f01dadc0e1863828897d52add2f90e6bd498f3
wzwleo <leo20050312@gmail.com>
2025-10-07 00:03:19 +0800

    Update solo_analysis.py

--- components/solo_analysis.py
+++ components/solo_analysis.py
@@ -47,7 +47,7 @@ def tab1_module():
             gap: 10px;                /* 上下間距 */
         ">
             <!-- 標題直排 -->
-            <div style= "background-color:#2a2a2a; "font-size:35px; font-weight:bold; color:#ffffff; text-align:center;">
+            <div style= "font-size:35px; font-weight:bold; color:#ffffff; text-align:center;">
                  {selected_row.get('標題','未提供')}
             </div>
             <div style="
