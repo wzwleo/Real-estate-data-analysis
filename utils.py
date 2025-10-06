47a8e9ea08537c4a9ddf8ae0e81008514338006a
wzwleo <leo20050312@gmail.com>
2025-10-06 23:10:35 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,8 +14,10 @@ def get_city_options(data_dir="./Data"):
 
     # 對照表：英文檔名 -> 中文名稱
     name_map = {
-        "Taichung-city_buy_properties.csv": "台中市","Taipei-city_buy_properties.csv": 台北市, 
-        "NewTaipei-city_buy_properties.csv": 新北市, "Keelung-city_buy_properties.csv": 基隆市, 
+        "Taichung-city_buy_properties.csv": "台中市",
+        "Taipei-city_buy_properties.csv": 台北市, 
+        "NewTaipei-city_buy_properties.csv": 新北市, 
+        "Keelung-city_buy_properties.csv": 基隆市, 
         "Yilan-county_buy_properties.csv": 宜蘭縣
         
 
