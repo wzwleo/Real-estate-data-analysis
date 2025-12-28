adef01690ff155e4b1030fe2388c0c7064c1dd2a
wzwleo <leo20050312@gmail.com>
2025-12-29 02:07:46 +0800

    Update search_page.py

--- page_modules/search_page.py
+++ page_modules/search_page.py
@@ -6,7 +6,7 @@ def render_search_page():
         """
         渲染搜尋頁面
         """
-        st.title("🔍 搜尋頁面2")
+        st.title("🔍 搜尋頁面")
         tab1, tab2, tab3 = st.tabs(["條件式搜尋", "AI 對話搜尋"])
         with tab1:
             # 渲染搜尋表單
