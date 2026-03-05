12721ec155bc841a2e910d6e0946533671ae796a
wzwleo <leo20050312@gmail.com>
2026-03-06 03:12:06 +0800

    Update solo_analysis.py

--- components/solo_analysis.py
+++ components/solo_analysis.py
@@ -1959,9 +1959,9 @@ def tab1_module():
                 with col1:
                     st.plotly_chart(fig)
                 with col2:
-                    st.write()
-                    st.write()
-                    st.write()
+                    st.write("\n")
+                    st.write("\n")
+                    st.write("\n")
                     st.markdown(
                         f"<h1 style='color:#00C853; font-size:60px; text-align:center;'>{total_score:.1f} / 100</h1>",
                         unsafe_allow_html=True
