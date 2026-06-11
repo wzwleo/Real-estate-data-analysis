cb71a45f3482fabe9e6473ed594d85d815e757b4
wzwleo <leo20050312@gmail.com>
2026-06-12 04:00:11 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -4,28 +4,28 @@ from page_modules.home_page import render_home_page
 from page_modules.search_page import render_search_page
 from page_modules.analysis_page import render_analysis_page
 from page_modules.analysis_records_page import render_analysis_records_page
-def main():
-#主應用程式入口
+from page_modules.cp_ranking_page import render_cp_ranking_page
 
+def main():
     st.set_page_config(layout="wide")
     
-    # 初始化頁面狀態
     if 'current_page' not in st.session_state:
         st.session_state.current_page = 'home'
-    # 渲染側邊欄
+
     render_sidebar()
     
-    # 頁面路由
     if st.session_state.current_page == 'home':
         render_home_page()
     elif st.session_state.current_page == 'search':
         render_search_page()
     elif st.session_state.current_page == 'analysis':
-         render_analysis_page()
+        render_analysis_page()
     elif st.session_state.current_page == 'compare':
         render_compare_page()
     elif st.session_state.current_page == 'records':
         render_analysis_records_page()
+    elif st.session_state.current_page == 'cp_ranking':
+        render_cp_ranking_page()
 
 if __name__ == "__main__":
     main()
